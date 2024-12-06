208c8769046bf0a3758b97bd1b140dcdf587866f
mustard221 <C24432034@mytudublin.ie>
2024-12-06 12:58:25 +0000

    fixing-beats

--- music-board/script/beat2.gd
+++ music-board/script/beat2.gd
@@ -1,5 +1,11 @@
-extends Button
+extends TextureButton
+
+var music_playing = false
 
 func _on_pressed() -> void:
-	$"Music-beat2-4".play()
-	pass # Replace with function body.
+	if music_playing:
+		$"Music-beat2-4".stop()
+		music_playing = false
+	else:
+		$"Music-beat2-4".play()
+		music_playing = true
--- music-board/script/beat3.gd
+++ music-board/script/beat3.gd
@@ -1,5 +1,11 @@
-extends Button
+extends TextureButton
+
+var music_playing = false
 
 func _on_pressed() -> void:
-	$"../Music-beat3-2".play()
-	pass # Replace with function body.
+	if music_playing:
+		$"Music-beat3-2".stop()
+		music_playing = false
+	else:
+		$"Music-beat3-2".play()
+		music_playing = true
