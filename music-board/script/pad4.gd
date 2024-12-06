8b170c7ffefb917cea691a232249a3af5ac5a140
mustard221 <C24432034@mytudublin.ie>
2024-12-06 13:22:15 +0000

    volume-slider-working-properly

--- music-board/script/pad4.gd
+++ music-board/script/pad4.gd
@@ -2,8 +2,8 @@ extends TextureButton
 var music_playing = false
 func _on_pressed() -> void:
 	if music_playing:
-		$"Music-pad4-8".stop()
+		$"Music-pad-4-8".stop()
 		music_playing = false
 	else:
-		$"Music-pad4-8".play()
+		$"Music-pad-4-8".play()
 		music_playing = true
--- music-board/script/volume_slider.gd
+++ music-board/script/volume_slider.gd
@@ -5,7 +5,7 @@ var master_bus = AudioServer.get_bus_index("Master")
 func _on_value_changed(new_value):
 	AudioServer.set_bus_volume_db(master_bus, new_value)
 	
-	if new_value == -30:
+	if new_value == 0:
 		AudioServer.set_bus_mute(master_bus, true)
 	else:
 		AudioServer.set_bus_mute(master_bus, false)
