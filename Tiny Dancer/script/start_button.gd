826521425571d6f6f7bbe5e80e6dd671c49f590b
mustard221 <C24432034@mytudublin.ie>
2024-12-07 19:48:01 +0000

    adding last changes

--- Tiny Dancer/script/start_button.gd
+++ Tiny Dancer/script/start_button.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://scenes/main scene.tscn")
 
 func _ready():
 	pass
